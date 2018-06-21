4179b75837f56f28d73311efa749c14403f35343
nirkaufman <nirkaufman@gmail.com>
2018-06-21 10:46:51 +0300

    cleanup

--- src/app/auth/services/auth.guard.ts
+++ src/app/auth/services/auth.guard.ts
@@ -1,11 +1,10 @@
 import {Injectable} from '@angular/core';
-import {CanActivate, Router} from '@angular/router';
+import {CanActivate} from '@angular/router';
 import {Observable} from 'rxjs/Observable';
 import {select, Store} from '@ngrx/store';
 import * as fromAuth from '../reducers/auth.reducer';
 import {map, take} from 'rxjs/internal/operators';
 import {LoginRedirect} from '../actions/auth.actions';
-
 
 @Injectable()
 export class AuthGuard implements CanActivate {
@@ -15,7 +14,7 @@ export class AuthGuard implements CanActivate {
   canActivate(): Observable<boolean> {
     return this.store.pipe(
       select(fromAuth.getLoogedIn),
-      map( authed => {
+      map(authed => {
         if (!authed) {
           this.store.dispatch(new LoginRedirect());
           return false;
